0f067cc1c92c67808faf1a3f546926e22305c8d8
Gaetanerk <gaetan.turben@outlook.com>
2024-10-17 11:30:40 +0200

    tests ok

--- src/main/java/exception/NotFoundException.java
+++ src/main/java/exception/NotFoundException.java
@@ -2,6 +2,6 @@ package exception;
 
 public class NotFoundException extends RuntimeException {
     public NotFoundException(String message) {
-        super("Le texte de la recherche contient moins de 2 caractères");
+        super(message);
     }
 }
--- src/main/java/org/example/Exercice4/RechercheVille.java
+++ src/main/java/org/example/Exercice4/RechercheVille.java
@@ -1,13 +1,32 @@
 package org.example.Exercice4;
 
-import jdk.jshell.spi.ExecutionControl;
+import exception.NotFoundException;
 
 import java.util.List;
+import java.util.ArrayList;
+import java.util.stream.Collectors;
 
 public class RechercheVille {
     private List<String> villes;
 
-    public List<String> rechercher(String mot) throws ExecutionControl.NotImplementedException {
-        throw new ExecutionControl.NotImplementedException("Not implemented yet");
+    public RechercheVille() {
+        villes = List.of("Paris", "Budapest", "Skopje", "Rotterdam", "Valence", "Vancouver", "Amsterdam", "Vienne", "Sydney", "New York", "Londres", "Bangkok", "Hong Kong", "Dubaï", "Rome", "Istanbul");
+    }
+
+    public List<String> rechercher(String mot) {
+        if (mot.length() < 2 && mot != "*") {
+            throw new NotFoundException("Le texte de recherche doit contenir au moins 2 caractères.");
+        }
+        if (mot.equals("*")) {
+            return new ArrayList<>(villes);
+        }
+        String lowerCaseMot = mot.toLowerCase();
+        return villes.stream()
+                .filter(ville -> ville.toLowerCase().contains(lowerCaseMot))
+                .collect(Collectors.toList());
+    }
+
+    public List<String> getVilles() {
+        return villes;
     }
 }
